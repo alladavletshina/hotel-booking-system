cb1d28be7dc8b5589d52466bed1abc8e5c7c0bc0
alladavletshina <atdavletshina@gmail.com>
2025-10-18 21:02:20 +0300

    something new

--- hotel-service-backup/src/main/java/com/hotelbooking/hotel/repository/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelbooking.hotel.repository;
-
-import com.hotelbooking.hotel.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
